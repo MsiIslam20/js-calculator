c60e1d14d15f4ff68ed65973792c26e434942b9a
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-07 20:19:17 +0600

    script update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,26 @@ var result = a + b;
 var diff = a - b;
 console.log(result);
 console.log(diff);
+
+
+//closers
+function stopWatch(){
+    let count = 0;
+    return function(){
+        count++
+        return count
+    }
+}
+
+const output = stopWatch();
+console.log(output());
+console.log(output());
+console.log(output());
+console.log(output());
+
+const output2 = stopWatch();
+console.log(output2());
+console.log(output2());
+console.log(output2());
+
+
